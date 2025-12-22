e834b10084b40e4ca778bc34fa5c90c28e64b27a
Rob Doneux <robertdoneux@gmail.com>
2025-12-22 08:18:38 +0000

    Remove dirty database handling logic from migration functions

--- services/migrations.go
+++ services/migrations.go
@@ -2,8 +2,6 @@ package services
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate/v4"
@@ -24,26 +22,6 @@ func RunMigrations() error {
 		return nil
 	}
 
-	if strings.HasPrefix(migrationErr.Error(), "Dirty database version") {
-		fmt.Println("Database is in a dirty state. Attempting to force clean...")
-
-		// Extract version number from error message
-		version, err := extractVersionFromDirtyError(migrationErr.Error())
-		if err != nil {
-			return fmt.Errorf("could not extract version from dirty error: %w", err)
-		}
-
-		// Force the database to clean state
-		err = forceDatabaseClean(version)
-		if err != nil {
-			return fmt.Errorf("could not force clean database: %w", err)
-		}
-
-		fmt.Println("Database forced to clean state. Retrying migrations...")
-		migrationErr = runMigrations("up")
-		return migrationErr
-	}
-
 	return migrationErr
 }
 
@@ -53,40 +31,6 @@ func RollbackMigrations() error {
 		return nil
 	}
 	return migrationErr
-}
-
-func extractVersionFromDirtyError(errMsg string) (int, error) {
-	// Error format: "Dirty database version 202512181751. Fix and force version."
-	parts := strings.Split(errMsg, " ")
-	if len(parts) < 4 {
-		return 0, fmt.Errorf("unexpected error format: %s", errMsg)
-	}
-
-	versionStr := strings.TrimSuffix(parts[3], ".")
-	version, err := strconv.Atoi(versionStr)
-	if err != nil {
-		return 0, fmt.Errorf("could not parse version number: %w", err)
-	}
-
-	return version, nil
-}
-
-func forceDatabaseClean(version int) error {
-	connectionString, err := config.GetDatabaseConfig().ConnectionString()
-	if err != nil {
-		return err
-	}
-
-	migration, err := migrate.New(
-		"file://migrations",
-		"mysql://"+connectionString,
-	)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Forcing database to clean state at version %d\n", version)
-	return migration.Force(version)
 }
 
 func runMigrations(direction string) error {
